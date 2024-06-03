860ba11a4e86a4725ae90496d20ed7ef2a1eb442
unknown <farshad.christ@gmail.com>
2024-06-03 21:21:56 +0300

    A class name Engine created for sdl setup and some important functions

--- main.cpp
+++ main.cpp
@@ -3,183 +3,86 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include<array>
+#include <array>
 #include <vector>
+#include "src/Engine.h"
 
 //-----------------
 #undef main
 //-----------------
 
 // global variables
-#define SCREEN_WIDTH 1260
-#define SCREEN_HEIGHT 800
-#define WHITE {255, 255, 255, 255}
-#define RED {255, 0, 0, 255}
-#define BLUE {0, 0, 255, 255}
-#define BLACK {0, 0, 0, 255}
-SDL_Renderer* renderer = nullptr;
-SDL_Window* window = nullptr;
+
+#define WHITE            \
+   {                     \
+      255, 255, 255, 255 \
+   }
+#define RED          \
+   {                 \
+      255, 0, 0, 255 \
+   }
+#define BLUE         \
+   {                 \
+      0, 0, 255, 255 \
+   }
+#define BLACK      \
+   {               \
+      0, 0, 0, 255 \
+   }
+
 bool quit = false;
 SDL_Event e;
-TTF_Font* gameplayFont = nullptr;
-
 
 int mouseX;
 int mouseY;
 
-SDL_Texture* backgroundTexture = nullptr;
-SDL_Texture* buttonPlay = nullptr;
+SDL_Texture *backgroundTexture = nullptr;
+SDL_Texture *buttonPlay = nullptr;
 
 // global functions
-bool Init()
-{
-   bool success = true;
 
-   if(SDL_Init(SDL_INIT_EVERYTHING) < 0 )
-   {
-      std::cerr << "SDL could not initialized!" << SDL_GetError() << std::endl;
-      success = false;
-   } 
-   if(IMG_Init(IMG_INIT_PNG) < 0)
-   {
-      std::cerr << "SDL_image could not initialized!" << IMG_GetError() << std::endl;
-      success = false;
-   } 
-   
-   if (TTF_Init() == -1)
-   {
-        std::cerr << "SDL_ttf could not initialize! TTF Error: " << TTF_GetError() << std::endl;
-        return false;
-   }
-
-   if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_WAVPACK) < 0)
-   {
-        std::cerr << "SDL_mixer could not initialize! Mixer Error: " << Mix_GetError() << std::endl;
-        return false;
-   }
-
-   // Open audio device
-   if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
-   {
-      std::cerr << "SDL_mixer could not open audio! Mixer Error: " << Mix_GetError() << std::endl;
-      return false;
-   }
-
-   // Allocate 16 mixing channels
-   Mix_AllocateChannels(16);
-
-   window = SDL_CreateWindow("Unknown!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-   if(window == nullptr)
-   {
-      std::cerr << "window not created!" << SDL_GetError() << std::endl;
-      success = false;
-   } 
-
-   renderer = SDL_CreateRenderer(window, -1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-   if(renderer == nullptr)
-   {
-      std::cerr << "renderer not created!" << SDL_GetError() << std::endl;
-      success = false;
-   } 
-
-   gameplayFont = TTF_OpenFont("assets/fonts/lazy.ttf", 28);
-   if(gameplayFont == nullptr)
-   {
-      std::cerr << "Failed to load gameplayfont! TTF Error: " << TTF_GetError() << std::endl;
-      success = false;
-   }
-
-   return success;
-}
-
-
-void GetCurrentMousePosition(int& x, int& y)
+void GetCurrentMousePosition(int &x, int &y)
 {
    SDL_GetMouseState(&x, &y);
 }
 
-void Close()
-{
-   SDL_DestroyRenderer(renderer);
-   SDL_DestroyWindow(window);
-   TTF_CloseFont(gameplayFont);
-
-
-   gameplayFont = nullptr;
-   renderer = nullptr;
-   window = nullptr;
-
-   SDL_Quit();
-   TTF_Quit();
-   IMG_Quit();
-}
 
 void RenderDefaultScreenColor()
 {
-   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-   SDL_RenderClear(renderer);
+   SDL_SetRenderDrawColor(Engine::GetInstance().GetRenderer(), 0, 0, 0, 255);
+   SDL_RenderClear(Engine::GetInstance().GetRenderer());
 }
-
 
 int main(int argc, char **argv)
 {
-   if(!Init())
+   if (!Engine::GetInstance().Init())
    {
       std::cerr << "something is wrong in initializing stuff in Init() function !\n";
    }
    else
    {
-       backgroundTexture = TextureManager::GetInstance().LoadTexture(renderer, "assets/bgmenu.png");
-       buttonPlay = TextureManager::GetInstance().LoadTexture(renderer, "assets/button.png");
+
+      backgroundTexture = TextureManager::GetInstance().LoadTexture(Engine::GetInstance().GetRenderer(), "assets/bgmenu.png");
+      buttonPlay = TextureManager::GetInstance().LoadTexture(Engine::GetInstance().GetRenderer(), "assets/button.png");
       while (!quit)
       {
          while (SDL_PollEvent(&e) != 0)
          {
-            if(e.type == SDL_QUIT)
+            if (e.type == SDL_QUIT)
             {
                quit = true;
             }
-            TextureManager::GetInstance().ShowTexture(renderer,backgroundTexture, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-            TextureManager::GetInstance().ShowTexture(renderer,buttonPlay, 200, 200, 120, 40);
+            TextureManager::GetInstance().ShowTexture(Engine::GetInstance().GetRenderer(), backgroundTexture, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            TextureManager::GetInstance().ShowTexture(Engine::GetInstance().GetRenderer(), buttonPlay, 200, 200, 120, 40);
          }
-         
 
          // update Render
-         SDL_RenderPresent(renderer);
+         SDL_RenderPresent(Engine::GetInstance().GetRenderer());
 
          // Simulate slower frame rate
-         //SDL_Delay(100); //Add 100ms delay per frame
+         // SDL_Delay(100); //Add 100ms delay per frame
       }
    }
-   Close();
-   return 0 ;  
+   Engine::GetInstance().Close();
+   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// GetCurrentMousePosition(mouseX, mouseY);
-// for(auto& var : rectArray)
-// {
-//    if(mouseX < var->x || mouseX > var->x + var->w )
-//    {
-
-//    }
-//    else if (mouseY < var->y || mouseY > var->y + var->h)
-//    {
-      
-//    }
-//    else
-//    {
-//       rectRef = var;
-//       break;
-//    }
-// }
---
+++ src/Engine.cpp
@@ -0,0 +1,85 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "Engine.h"
+#include <iostream>
+#include <string>
+
+bool Engine::Init()
+{
+    bool success = true;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cerr << "SDL could not initialized!" << SDL_GetError() << std::endl;
+        success = false;
+    }
+    if (IMG_Init(IMG_INIT_PNG) < 0)
+    {
+        std::cerr << "SDL_image could not initialized!" << IMG_GetError() << std::endl;
+        success = false;
+    }
+
+    if (TTF_Init() == -1)
+    {
+        std::cerr << "SDL_ttf could not initialize! TTF Error: " << TTF_GetError() << std::endl;
+        return false;
+    }
+
+    if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_WAVPACK) < 0)
+    {
+        std::cerr << "SDL_mixer could not initialize! Mixer Error: " << Mix_GetError() << std::endl;
+        return false;
+    }
+
+    // Open audio device
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+    {
+        std::cerr << "SDL_mixer could not open audio! Mixer Error: " << Mix_GetError() << std::endl;
+        return false;
+    }
+
+    // Allocate 16 mixing channels
+    Mix_AllocateChannels(16);
+
+    Engine &instance = Engine::GetInstance();
+
+    instance.m_Window = SDL_CreateWindow("Unknown!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (instance.m_Window == nullptr)
+    {
+        std::cerr << "window not created!" << SDL_GetError() << std::endl;
+        success = false;
+    }
+
+    instance.m_Renderer = SDL_CreateRenderer(instance.m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (instance.m_Renderer == nullptr)
+    {
+        std::cerr << "renderer not created!" << SDL_GetError() << std::endl;
+        success = false;
+    }
+
+    instance.m_GameplayFont = TTF_OpenFont("assets/fonts/lazy.ttf", 28);
+    if (instance.m_GameplayFont == nullptr)
+    {
+        std::cerr << "Failed to load gameplayfont! TTF Error: " << TTF_GetError() << std::endl;
+        success = false;
+    }
+
+    return success;
+}
+
+void Engine::Close()
+{
+    Engine& instance = Engine::GetInstance();
+    SDL_DestroyRenderer(instance.m_Renderer);
+    SDL_DestroyWindow(instance.m_Window);
+    TTF_CloseFont(instance.m_GameplayFont);
+
+    instance.m_Renderer = nullptr;
+    instance.m_Window = nullptr;
+    instance.m_GameplayFont = nullptr; 
+
+    SDL_Quit();
+    TTF_Quit();
+    IMG_Quit();
+}
